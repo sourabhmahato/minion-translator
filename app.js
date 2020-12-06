1666576b3d04306ff589c67ac0594d76be597698
Sourabh Mahato <sourabhmahato5701@gmail.com>
2020-12-06 20:34:14 +0530

    added background image

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 
 function clickHandler() {
-    console.log("clicked.");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "translated" + txtInput.value;
 };
 btnTranslate.addEventListener("click", clickHandler)
